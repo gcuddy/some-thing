b468774f8c1f4318ecb88f243d579de7b6b6e0dd
Gus <gus.cuddy@gmail.com>
2024-01-25 20:40:38 -0500

    browser

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -3,6 +3,7 @@ import { createReplicache } from './replicache'
 
 export async function load() {
 	console.log('load layout')
+    console.log('browser', browser)
 	if (browser) {
 		console.log('load layout browser')
 		const replicache = createReplicache()
